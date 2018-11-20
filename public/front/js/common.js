efc97d8e8b599cb1aedaf6f4871137bf28bceb49
avalonwb <avalonwb@qq.com>
2018-11-20 17:35:29 +0800

    第二遍移动端开始

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -5,3 +5,28 @@ mui('.mui-scroll-wrapper').scroll({
   indicators: false
 });
 
+// 封装获取地址栏数据的方法
+
+function getData( key ) {
+    
+   var str = location.search;  
+   var obj = {};
+
+   str = decodeURI(str);
+   // ?key=月&name=12
+   str = str.slice(1);
+   // key=月&name=12
+   var arr = str.split("&");
+   // ["key=月", "name=12"]
+   arr.forEach(function (ele,index) {
+     var k = ele.split("=")[0];
+     var v = ele.split("=")[1];
+
+     obj[k] = v;
+   });
+   //{key: "月", name: "12"} 
+
+   return obj[key];
+     
+}
+
